9da1c8f616e0b7968a6a6625aced61303b9627be
Sébastien Paquet <shrymex@gmail.com>
2016-03-30 15:23:22 -0400

    Added test for http

--- dev/app/services/user.spec.js
+++ dev/app/services/user.spec.js
@@ -1,13 +1,51 @@
-System.register([], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/http', 'angular2/testing', 'angular2/http/testing', './user.service', 'rxjs/Rx', 'rxjs/add/operator/map'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
+    var core_1, http_1, testing_1, testing_2, user_service_1;
     return {
-        setters:[],
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (testing_1_1) {
+                testing_1 = testing_1_1;
+            },
+            function (testing_2_1) {
+                testing_2 = testing_2_1;
+            },
+            function (user_service_1_1) {
+                user_service_1 = user_service_1_1;
+            },
+            function (_1) {},
+            function (_2) {}],
         execute: function() {
-            describe('UserService', function () {
-                it('has id given in the constructor', function () {
-                    expect(1).toEqual(1);
-                });
+            testing_1.describe('UserService', function () {
+                testing_1.beforeEachProviders(function () { return [
+                    user_service_1.UserService,
+                    http_1.BaseRequestOptions,
+                    testing_2.MockBackend,
+                    core_1.provide(http_1.Http, {
+                        useFactory: function (backend, defaultOptions) {
+                            return new http_1.Http(backend, defaultOptions);
+                        },
+                        deps: [testing_2.MockBackend, http_1.BaseRequestOptions]
+                    })
+                ]; });
+                testing_1.beforeEach(testing_1.inject([testing_2.MockBackend], function (backend) {
+                    var baseResponse = new http_1.Response(new http_1.ResponseOptions({ body: { id: 1, email: 'a@a.com', username: 'a@a.com', password: 'a@a.com' } }));
+                    backend.connections.subscribe(function (c) { return c.mockRespond(baseResponse); });
+                }));
+                testing_1.it('should return user when connect worked', testing_1.inject([user_service_1.UserService], function (userService) {
+                    userService.connect('a@a.com', 'a@a.com').subscribe(function (res) {
+                        testing_1.expect(res.id).toBe(1);
+                        testing_1.expect(res.email).toBe('a@a.com');
+                        testing_1.expect(res.password).toBe('a@a.com');
+                        testing_1.expect(res.username).toBe('a@a.com');
+                    });
+                }));
             });
         }
     }
--- dev/app/services/user.spec.ts
+++ dev/app/services/user.spec.ts
@@ -1,9 +1,47 @@
+import {provide} from 'angular2/core';
+
+import {BaseRequestOptions, Response, ResponseOptions, Http, Headers} from 'angular2/http';
+import {it, describe, expect, inject, injectAsync, beforeEachProviders, beforeEach} from 'angular2/testing';
+import {MockBackend, MockConnection} from 'angular2/http/testing';
+
+import {Injectable, Component} from 'angular2/core';
+
 import {UserService} from './user.service';
+import {User} from '../objects/user';
+
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 describe('UserService', () => {
 
-	it('has id given in the constructor', () => {
-		expect(1).toEqual(1);
-	});
+	beforeEachProviders(() => [
+		UserService,
+		BaseRequestOptions,
+		MockBackend,
+		provide(Http, {
+			useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+				return new Http(backend, defaultOptions);
+			},
+			deps: [MockBackend, BaseRequestOptions]
+		})
+	]);
+
+	beforeEach(inject([MockBackend], (backend: MockBackend) => {
+		const baseResponse = new Response(new ResponseOptions({ body: { id: 1, email: 'a@a.com', username: 'a@a.com', password: 'a@a.com' } }));
+		backend.connections.subscribe((c: MockConnection) => c.mockRespond(baseResponse));
+	}));
+
+	it('should return user when connect worked',
+		inject([UserService], (userService: UserService) => {
+			userService.connect('a@a.com', 'a@a.com').subscribe((res: User) => {
+				expect(res.id).toBe(1);
+				expect(res.email).toBe('a@a.com');
+				expect(res.password).toBe('a@a.com');
+				expect(res.username).toBe('a@a.com');
+			});
+		})
+	);
+
+
 
 });
