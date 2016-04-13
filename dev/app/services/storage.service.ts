c6c7199120ee99b820c4ce9788c449d0e858fb8f
Sébastien Paquet <shrymex@gmail.com>
2016-04-13 15:06:30 -0400

    Added service to use session/localStorage easily

---
+++ dev/app/services/storage.service.ts
@@ -0,0 +1,46 @@
+import {Injectable, Component} from 'angular2/core';
+
+
+
+@Injectable()
+
+export class StorageService {
+
+	set(key: string, value: any) {
+		sessionStorage.setItem(key, JSON.stringify(value));
+	}
+
+	get(key: string) {
+		let sessionStr: string = sessionStorage.getItem(key);
+		let sessionObj: any;
+
+		try {
+			sessionObj = JSON.parse(sessionStr);
+			return sessionObj;
+		} catch (e) {
+			return sessionStr;
+		}
+
+	}
+
+
+}
+
+export class PermanentStorageService {
+	set(key: string, value: any) {
+		localStorage.setItem(key, JSON.stringify(value));
+	}
+
+	get(key: string) {
+		let localStr: string = localStorage.getItem(key);
+		let localObj: any;
+
+		try {
+			localObj = JSON.parse(localStr);
+			return localObj;
+		} catch (e) {
+			return localStr;
+		}
+
+	}
+}
